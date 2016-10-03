7ed1120e639c8f912877bca9b884e21f49d80ff9
AlexisRoj <alexisroj2@gmail.com>
2016-10-03 07:26:47 -0600

    Se mejora menu visualmente

---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/projectunitthree/fragments/SwipeDesplazamiento.java
@@ -0,0 +1,36 @@
+package com.innovagenesis.aplicaciones.android.projectunitthree.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Clase encargada de adnistrar el desplazamiento
+ */
+
+public class SwipeDesplazamiento extends FragmentPagerAdapter {
+
+    public SwipeDesplazamiento(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0){
+            return new ProgramacionAyer();
+        }
+        if(position == 1){
+            return new ProgramacionHoy();
+        }
+        else{
+            return new ProgramacionManana();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
